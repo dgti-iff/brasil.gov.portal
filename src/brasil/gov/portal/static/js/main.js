7691903e4bb17a293193b6bb8c46f681d8a2e3a3
Tiago Samaha <tiagosamaha@gmail.com>
2015-11-18 12:06:44 -0200

    removendo label do banner rotativo

--- src/brasil/gov/portal/static/js/main.js
+++ src/brasil/gov/portal/static/js/main.js
@@ -260,6 +260,7 @@ jQuery(document).ready(function ($) {
     $(".portletWrapper:nth-child(n+4) dd").each(function(){
       $(this).css("display", "none");
     });
+    $("#tile_banner_rotativo span.title").css("display", "none");
 });
 
 $(window).load(function() {
