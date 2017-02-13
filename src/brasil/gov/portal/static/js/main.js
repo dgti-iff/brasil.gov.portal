703c3f52b01a73a069f37aca7f7c16922dfec6ea
whanderley <whanderley.souza@gmail.com>
2017-02-13 12:09:44 -0200

    Update main.js

--- src/brasil/gov/portal/static/js/main.js
+++ src/brasil/gov/portal/static/js/main.js
@@ -297,6 +297,6 @@ $(window).load(function() {
         $("#em-destaque").toggleClass("closed opened");
         setTimeout(setaDisplay, 200);
     })
-    $("#em-destaque-titulo").css("font-size","24px");
+    $("#em-destaque-titulo").attr('style',"font-size: 24px !important;");
 });
 
